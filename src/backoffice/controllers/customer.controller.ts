564e5817043a7eb2270f909e23f2dfa39e718226
Leonardo Tomas Costa da Silva <76014605+leonardotomascostadasilva@users.noreply.github.com>
2021-02-24 18:48:54 -0300

    feat: criação das rotas de customers

---
+++ src/backoffice/controllers/customer.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+
+@Controller('v1/customers')
+export class CustomerController {
+  
+    @Get()
+    get() {
+    return 'Obter os clientes';
+  }
+
+  @Post()
+  post() {
+    return 'Criar um cliente';
+  }
+
+  @Put()
+  put() {
+    return 'Atualizar um cliente';
+  }
+
+  @Delete()
+  delete() {
+    return 'Deletar um cliente';
+  }
+}
